c0d7976a8601c52d8671afcf231c170fce478537
CodPatrik <54668490+CodPatrik@users.noreply.github.com>
2020-02-07 12:50:47 +0100

    Add files via upload

---
+++ template/js/script.js
@@ -0,0 +1,15 @@
+
+/*
+I denna fil skrivs all javascript dvs instruktioner till datorn
+eller "program" som man brukar kalla det.
+
+Tex. en funktion
+*/
+
+
+function runProgram() {
+    alert('Du tryckte på <div> taggen');
+}
+
+
+
