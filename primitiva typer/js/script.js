13cfdbc50787acc1a0e8180976ad695b856fdfe3
CodPatrik <54668490+CodPatrik@users.noreply.github.com>
2020-02-07 14:53:29 +0100

    Add files via upload

---
+++ primitiva typer/js/script.js
@@ -0,0 +1,35 @@
+
+// Olika variabler i javascript och dess datatyper
+
+let name = "Patrik";      //String
+let age = 30;             //Integer
+let volume = 3.5;            //float
+let isNotApproved = false;   //Boolean
+let isApproved = true;    //Boolean
+let lastName = undefined; //Ingen datatyp alls variabel är tom eller ej definierad
+lastName = null;      //Används då man vill se till att en variabel är tom på innehåll.
+
+// Man kan visa innehållet i en variabel med en inbyggd funktion som finns i Javascript
+// den heter console.log() och används i verkligheten oftast av programutvecklare för att
+// se att programmet har gjort vad man vill utan att man visar det för användaren.
+// vissa program har logglistor där man kan se alla kommandon som en användare gjort tex.
+// Här kan vi använda det för att se i konsollen att det händer det som vi vill ska hända 
+// när vi tränar på programmering.
+
+//Konsollen hittar ni om man i webbläsaren väljer att klicka på högerknapp på pekdon och väljer
+//Inspektera. Denna funktion är bortkopplad av den som styr webbläsaren om ni är synkade med skolans system
+//på google chrome. Därför har vi här valt Firefox som webbläsare för att slippa logga ut o in på google.
+
+//För att skriva ut variabel age som innehåller talet 30 så skriver man således detta.
+
+console.log(age);
+
+
+
+
+
+
+
+
+
+
